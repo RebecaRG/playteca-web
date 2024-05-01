0b8d03dd0fef36c96d3a3c10d7aa044ee0e6adc9
Rebeca Rodríguez <rebeca.rodriguez.guirao@gmail.com>
2024-05-01 23:58:54 +0200

    update2 main and app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,33 +1,12 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideToastr } from 'ngx-toastr';
-import { FeatherModule } from 'angular-feather';
-import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { allIcons } from 'angular-feather/icons';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-
+import { HttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes), 
-    provideAnimations(), 
-    provideToastr(),
-    provideHttpClient(withInterceptorsFromDi()),
-    importProvidersFrom(CommonModule, BrowserModule, NgbModule, FeatherModule, FeatherModule.pick(allIcons), TranslateModule.forRoot({
-                  loader: {
-                    provide: TranslateLoader,
-                    useFactory: HttpLoaderFactory,
-                    deps: [HttpClient],
-                  },
-                })),
-  ]
+  providers: [provideRouter(routes)]
 };
 
 export function HttpLoaderFactory(http: HttpClient) {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,34 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import '@angular/localize/init';
+import { HttpLoaderFactory } from './app/app.config';
+import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
+import { allIcons } from 'angular-feather/icons';
+import { FeatherModule } from 'angular-feather';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { withInterceptorsFromDi, provideHttpClient, HttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
-bootstrapApplication(AppComponent, appConfig)
-    .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(CommonModule, BrowserModule, NgbModule, FeatherModule, FeatherModule.pick(allIcons), TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient],
+            },
+        })),
+        provideAnimations(),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideRouter(routes),
+        provideHttpClient(withInterceptorsFromDi()),
+    ]
+})
+  .catch(err => console.error(err));
