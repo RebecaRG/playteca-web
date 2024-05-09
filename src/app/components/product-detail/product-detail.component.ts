fcc35f7efa7c5b3838e96fdd9fb50c19402166eb
Rebeca Rodríguez <rebeca.rodriguez.guirao@gmail.com>
2024-05-09 13:34:48 +0200

    implement goBack()

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Product, ResponseProducts } from '../../interfaces/product';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ProductService } from '../../services/product.service';
+import { Location } from '@angular/common';
 
 
 @Component({
@@ -17,11 +18,16 @@ export class ProductDetailComponent {
 
   constructor(
     private route: ActivatedRoute,
-    private productService: ProductService
+    private productService: ProductService,
+    private location: Location
   ) { }
 
   ngOnInit(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.productService.getProduct(id).subscribe(product => this.product = product);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
